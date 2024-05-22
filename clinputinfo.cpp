c0345dfa011b8d01605354df0c72c20392d6fc58
Sivuyile Fodi <sivuyile.fodi@s4.co.za>
2024-05-22 15:34:11 +0200

    completed application

--- clinputinfo.cpp
+++ clinputinfo.cpp
@@ -1,5 +1,7 @@
 #include "clinputinfo.h"
 #include "ui_clinputinfo.h"
+#include <QStringList>
+#include <QMessageBox>
 
 clInputInfo::clInputInfo(QWidget *parent) :
     QDialog(parent),
@@ -16,7 +18,35 @@ clInputInfo::~clInputInfo()
 
 void clInputInfo::slotSaveBtnClicked()
 {
-    std::cout << "save btn pressed" << std::endl;
+    QString loInfo = ui->meEdtInput->text();//validation of the
+    QStringList loList(loInfo.split(" "));
+    QString loInitials = "";
+    QString loGenKey = "";
+    int loGenKeySize = 0;
+    QMessageBox msg;
+    msg.setWindowTitle("Message");
+    msg.setIcon(QMessageBox::Information);
+
+
+    if (loList.size() >= 2)
+    {
+        foreach (QString item, loList)
+        {
+            loInitials.append(item.left(1));
+            loGenKeySize = loGenKeySize + item.count();
+        }
+        loGenKey.append(loInitials.left(2));
+        loGenKey = loGenKey.append(QString::number(loGenKeySize));
+        loGenKey = loGenKey.leftJustified(6,'0');
+
+        msg.setText(loGenKey);
+        msg.exec();
+    }
+    else
+    {
+        msg.setText("Enter correct full name");
+        msg.exec();
+    }
 }
 
 void clInputInfo::slotCancelBtnClicked()
